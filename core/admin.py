c81ca9f8bf31c89436f9635dbbd289c05287649a
Karen <najafzadehkaren@gmail.com>
2023-10-14 22:13:49 +0330

    Update admin.py

--- core/admin.py
+++ core/admin.py
@@ -10,11 +10,17 @@ class UserAdmin(BaseUserAdmin):
             'fields': ('username', 'password1', 'password2', 'email', 'first_name', 'last_name'),
         }),
     )
+    list_display = ['username', 'email', 'first_name', 'last_name', 'is_staff', 'is_superuser']
+    search_fields = ['username', 'first_name', 'last_name']
 
 @admin.register(Librarian)
 class LibrarianAdmin(admin.ModelAdmin):
-    list_display = ['student_number', 'phone_number', 'user']
+    list_display = ['student_number', 'user', 'phone_number' ]
+    search_fields = ['user__first_name', 'user__last_name','student_number']
 
 @admin.register(Shift)
 class ShiftAdmin(admin.ModelAdmin):
     list_display = ['librarian','day', 'start_time', 'end_time']
+    search_fields = ['librarian__user__first_name']
+    list_filter = ['day','librarian']
+    ordering = ['day', 'start_time']
