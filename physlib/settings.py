4ffcd030a4007419b0f3c54c7bdd52126f856229
Karen <najafzadehkaren@gmail.com>
2023-10-19 22:19:54 +0330

    changed the database to mysql

--- physlib/settings.py
+++ physlib/settings.py
@@ -80,8 +80,11 @@ WSGI_APPLICATION = 'physlib.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'physlib',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': '1234'
     }
 }
 
