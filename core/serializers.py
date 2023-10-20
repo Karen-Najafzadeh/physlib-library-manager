12514a6204db276fdb620f9a430c2159f1b6c337
Karen <najafzadehkaren@gmail.com>
2023-10-20 14:13:23 +0330

    done some minor changes in librarian serializer

--- core/serializers.py
+++ core/serializers.py
@@ -1,10 +1,12 @@
 from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from . models import * 
 
 class LibrarianSerializer(ModelSerializer):
+    user = serializers.StringRelatedField()
     class Meta:
         model = Librarian
-        fields = '__all__'
+        fields = ['student_number', 'phone_number', 'user']
 
 class ShiftSerializer(ModelSerializer):
     class Meta:
