6ad3f0135047536d502b5a89fd0d26a22df45746
Karen <najafzadehkaren@gmail.com>
2023-10-20 22:15:20 +0330

    done with the locker booking system

--- library/urls.py
+++ library/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework import routers
+from rest_framework_nested import routers as nested_routers
 from . import views
 
 router = routers.DefaultRouter()
@@ -7,6 +8,13 @@ router.register('books', views.BookViewset)
 router.register('members', views.MemberViewset)
 router.register('lockers', views.LockerViewset)
 
+n_router = nested_routers.NestedDefaultRouter(router, 'lockers', lookup = 'locker')
+n_router.register('booking', views.Booking)
+
+income_nested_router = nested_routers.NestedDefaultRouter(n_router, 'booking', lookup='new_income')
+income_nested_router.register('pricing', views.Pricing)
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('',include(n_router.urls)),
+    path('',include(income_nested_router.urls)),
 ]
--- library/views.py
+++ library/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, redirect
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet 
+from rest_framework.generics import CreateAPIView,ListAPIView
 from django_filters import rest_framework as filter
 from .models import *
+from finance.models import Income
+from finance.serializers import IncomeSerializer
 from .serializers import *
 from .filters import *
 from .pagination import DefaultPagination
@@ -43,3 +46,38 @@ class LockerViewset(ModelViewSet):
     queryset = Locker.objects.all()
     serializer_class = LockerSerializer
     filterset_class = LockerFilter
+
+    @action(detail=True)
+    def book_now(self, *args, **kwargs):
+        return redirect (f"http://127.0.0.1:8000/library/lockers/{self.kwargs['pk']}/booking/")
+
+class Booking(ModelViewSet):
+    queryset = Member.objects.all()
+    serializer_class = MemberSerializer
+
+    @action(detail=True)
+    def book_for_the_current_member(self, *args, **kwargs):
+        return redirect(f"http://127.0.0.1:8000/library/lockers/{self.kwargs['locker_pk']}/booking/{self.kwargs['pk']}/pricing/")
+        # print(f'initial {self.kwargs}\n\n{kwargs}\n\n{args}')
+        locker = Locker.objects.get(pk=self.kwargs['locker_pk'])
+        student_id = Member.objects.get(pk = self.kwargs['pk'])
+        print(f'queries {student_id.pk}\n\n {locker.pk} and {locker.student}')
+        locker.student = student_id
+        locker.save()
+        # print(f'locker {locker.student}')
+        return Response(f'{locker.pk} {locker.cost} {locker.student}')
+
+class Pricing(ModelViewSet):
+    queryset = Income.objects.all()
+    def list(self, *args, **kwargs):
+        return Response('create a new income object')
+    def create(self,request, *args, **kwargs):
+        locker = Locker.objects.get(pk=self.kwargs['locker_pk'])
+        student_id = Member.objects.get(pk = self.kwargs['new_income_pk'])
+        locker.student = student_id
+        locker.save()
+        serializer = IncomeSerializer(data = request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    serializer_class = IncomeSerializer
